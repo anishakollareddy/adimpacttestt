75dbaa74edaa0077d9a51a898eed675716f2261a
Anisha <bluestar225@gmail.com>
2020-11-24 01:56:32 -0800

    ugh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import SurveyOne from "./components/SurveyOne";
-import SurveyTwo from "./components/SurveyTwo";
+// import SurveyOne from "./components/SurveyOne";
+// import SurveyTwo from "./components/SurveyTwo";
 import Title from './Title';
 import { Router, Route, Switch } from "react-router-dom";
 import history from "./utils/history";
@@ -12,16 +12,19 @@ function App() {
     {/* Don't forget to include the history module */}
     <Title />
 
-    <Router history={history}>
-      <Switch>
-        <Route path="/surveyone" component={SurveyOne} />
-        <Route path="/surveytwo" component={SurveyTwo} />
-      </Switch>
-    </Router>
+
   </div>
   );
 }
 
 export default App;
+
+/*
+    <Router history={history}>
+      <Switch>
+        <Route path="/surveyone" component={SurveyOne} />
+        <Route path="/surveytwo" component={SurveyTwo} />
+      </Switch>
+    </Router>*/
 
 
